ba727bdc43cfc6754cc2524b568e439133b21ca2
UTAN25 <unaitel25@gmail.com>
2021-12-11 13:11:31 +0100

    update

---
+++ GccApplication2/GccApplication2/main.c
@@ -0,0 +1,18 @@
+/*
+ * GccApplication2.c
+ *
+ * Created: 11/12/2021 11:40:01
+ * Author : ander
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
