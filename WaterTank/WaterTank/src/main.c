1a86de3493023e1f878dec9cc75b8fc697bda643
UTAN25 <unaitel25@gmail.com>
2021-12-12 11:19:24 +0100

    update

--- WaterTank/WaterTank/src/main.c
+++ WaterTank/WaterTank/src/main.c
@@ -15,6 +15,8 @@
 #include <stdlib.h>         // C library. Needed for conversion function
 #include "bme280.h"			//add Library to control the BME
 #include "hc-sr04.h"
+#include "bme280.c"		
+#include "hc-sr04.c"
 #include <math.h>
 
 
@@ -26,7 +28,7 @@ uint8_t setting = 0;	//Defines what the LCD while display
 bme280_init();
 init_ultrasonic_sensor();
 lcd_init(LCD_DISP_ON);
-sei();
+
 
 /* Function definitions ----------------------------------------------*/
 /**********************************************************************
