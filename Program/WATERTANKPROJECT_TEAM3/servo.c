1c2f84c9f1fb12f4920b86bfd45835163068c575
Unai Telletxea <34486955+UTAN25@users.noreply.github.com>
2021-12-16 17:27:39 +0100

    Update servo.c

--- Program/WATERTANKPROJECT_TEAM3/servo.c
+++ Program/WATERTANKPROJECT_TEAM3/servo.c
@@ -20,15 +20,13 @@
 
 void setupServo( uint8_t porcentage)
 {
-	lcd_init(LCD_DISP_OFF);															// We initialize the lcd value
-	DDRD |= (1<<PD5);																// Fast PWM output at OC0B pin
+	lcd_init(LCD_DISP_OFF);								// We turn off the lcd
+	DDRD |= (1<<PD5);								// Fast PWM output at OC0B pin
 	
-	OCR0A  = 255;																	// Frequency 1 kHz
-																					// Top Value of 200(must be equal or greater than Duty Cycle)
-	OCR0B = round(255-256*(0.030517*porcentage+9.15527)/100);						// Duty cycle of 75%
-	TCCR0A |= (1<<COM0B1) | (1<<COM0B0) | (1<<WGM01) | (1<<WGM00);					// Inverting Fast PWM mode 7
-	//TCCR0A |= (1<<COM0B1) | (1<<WGM01) | (1<<WGM00);	//Non-Inverting Fast PWM mode 7
+	OCR0A  = 255;			
+	OCR0B = round(255-256*(0.030517*porcentage+9.15527)/100);		// The duty cicle formula depending on the percentage
+	TCCR0A |= (1<<COM0B1) | (1<<COM0B0) | (1<<WGM01) | (1<<WGM00);			// Inverting Fast PWM mode 7
 	TCCR0B |= (1<<WGM02) | (1<<CS00);
-	TCCR0B |= (1<<WGM02) | (1<<CS02);												// No-Prescalar
+	TCCR0B |= (1<<WGM02) | (1<<CS02);					// Prescalar 1024
 
 }
