408971a61a9072d964c4aca127db12e87943be65
Unai Telletxea <34486955+UTAN25@users.noreply.github.com>
2021-12-16 17:32:44 +0100

    Update menu.c

--- Program/WATERTANKPROJECT_TEAM3/menu.c
+++ Program/WATERTANKPROJECT_TEAM3/menu.c
@@ -19,125 +19,125 @@ void Display(uint8_t setting, uint16_t data[], uint8_t FULL){
 	char lcd_string[2] = " ";						// Creates a string
 	lcd_gotoxy(0,1);
 	
-	switch (setting)								// Defines the display of each setting
+	switch (setting)						// Defines the display of each setting
 	{
 		case 0:										// Depth
 		data[0] = DistanceSensorValue(FULL);		// Update the water level
-		lcd_gotoxy(0,0);							// We got to the 0,0
+		lcd_gotoxy(0,0);							// We go to the 0,0
 		lcd_puts("Depth:");							// We write the text, in this case is Depth
 		if(data[0]>=FULL){							// If the depth is the same as the full tank we write a message of FULL
 				lcd_gotoxy(0,1);					// We move to the 0,1 position and we write the message
 				lcd_puts("FULL");
 		}else{
-		lcd_gotoxy(0,1);							// If the value is not the same as the full tank, we write the depht in the 0,1 position
+		lcd_gotoxy(0,1);						// If the value is not the same as the full tank, we write the depth in the 0,1 position
 		itoa(data[setting],lcd_string,10);			// We transform the value from the data matrix into a string
-		lcd_puts(lcd_string);						// We write the string
-		lcd_gotoxy(3,1);							// As the value in real simulation is always going to be less or same as 400 is going to the 3 digits so we clean the 4th
-		lcd_puts(" ");								// We write an space in the 3,1 position
-		if(data[setting]<=999){						// We need to do some corrections, depending of the digits
-			lcd_gotoxy(3,1);						// If the value is 3 digits we clean the 4th with a space
+		lcd_puts(lcd_string);				// We write the string
+		lcd_gotoxy(3,1);		// As the value in real simulation is always going to be less or same as 400 is going to the 3 digits so we clean the 4th
+		lcd_puts(" ");						// We write an space in the 3,1 position
+		if(data[setting]<=999){					// We need to do some corrections, depending of the digits
+			lcd_gotoxy(3,1);				// If the value is 3 digits we clean the 4th with a space
 			lcd_puts(" ");
 		}
 		if(data[setting]<=99){
-			lcd_gotoxy(2,1);						// If the value is 2 digits we clean the 3th with a space too
+			lcd_gotoxy(2,1);				// If the value is 2 digits we clean the 3th with a space too
 			lcd_puts(" ");
 		}
 		if(data[setting]<=9){
-			lcd_gotoxy(1,1);						// If the value is 1 digit we clean the 2nd with a space too
+			lcd_gotoxy(1,1);				// If the value is 1 digit we clean the 2nd with a space too
 			lcd_puts(" ");
 		}
 		lcd_gotoxy(5,1);
-		lcd_puts("cm");								// We put the unit
+		lcd_puts("cm");						// We put the unit
 		}
 		break;
 		
-		case 1:										// Valve open ratio
+		case 1:							// Valve open ratio
 		itoa(data[setting],lcd_string,10);			// The open percentage is stored in the data matrix, so we need to convert it into a string
-		lcd_puts(lcd_string);						// Write the string
-		if(data[setting]<=95){						// As the value increases from 5 to 5, if the value is higher than 95 we clean the 3th digit
-			lcd_gotoxy(2,1);						// We put an space in the 2,1 position
+		lcd_puts(lcd_string);					// Write the string
+		if(data[setting]<=95){					// As the value increases from 5 to 5, if the value is higher than 95 we clean the 3th digit
+			lcd_gotoxy(2,1);				// We put an space in the 2,1 position
 			lcd_puts(" ");
 		}
-		if(data[setting]==5){						// If its 5 so its 1 digit, so we clean
-			lcd_gotoxy(1,1);						// We put an space at 1,1 position
+		if(data[setting]==5){					// If its 5 so its 1 digit, so we clean
+			lcd_gotoxy(1,1);				// We put an space at 1,1 position
 			lcd_puts(" ");
 		}
-		lcd_gotoxy(0,0);							// We write Valve in the 0,0 position
+		lcd_gotoxy(0,0);					// We write Valve in the 0,0 position
 		lcd_puts("Valve:");
-		lcd_gotoxy(5,1);							// We write the unit in the 5,1 position
+		lcd_gotoxy(5,1);					// We write the unit in the 5,1 position
 		lcd_puts("%");
 		break;
 		
-		case 2:										// Pump state
+		case 2:						// Pump state
 		itoa(data[setting],lcd_string,10);			// The pump state is stored in the data matrix, so we need to convert it into a string
-		if(data[setting]==1){						// If it is 1, the pump is On, so we write On in the display
+		if(data[setting]==1){					// If it is 1, the pump is On, so we write On in the display
 			lcd_puts("On ");
 
 		}
-		else{										// If it is 0, the pump is Off, so we write Off in the display
+		else{							// If it is 0, the pump is Off, so we write Off in the display
 			lcd_puts("Off");
 		}
-		lcd_gotoxy(0,0);							// We go to the 0,0 position
-		lcd_puts("Pump:");							// And we write Pump in that position
+		lcd_gotoxy(0,0);					// We go to the 0,0 position
+		lcd_puts("Pump:");					// And we write Pump in that position
 		break;
 		
-		case 3:										// Atmosphere Pressure
-		data[3] = PressureGetValue(data, FULL);		// Update the pressure at the bottom of the tank
+		case 3:							// Atmosphere Pressure
+		data[3] = PressureGetValue(data, FULL);			// Update the pressure at the bottom of the tank
 		itoa(data[setting],lcd_string,10);			// We convert it into a string
-		lcd_puts(lcd_string);						// We write this string
-		if(data[setting]<=999){						// If the value is 3 digits we clean the 4th one
-			lcd_gotoxy(3,1);						// By putting a space at 3,1 position
+		lcd_puts(lcd_string);					// We write this string
+		if(data[setting]<=999){					// If the value is 3 digits we clean the 4th one
+			lcd_gotoxy(3,1);				// By putting a space at 3,1 position
 			lcd_puts(" ");
 		}
-		if(data[setting]<=99){						// If the value is 2 digits we clean the 3th one
-			lcd_gotoxy(2,1);						// By putting a space at 2,1 position
+		if(data[setting]<=99){					// If the value is 2 digits we clean the 3th one
+			lcd_gotoxy(2,1);				// By putting a space at 2,1 position
 			lcd_puts(" ");
 		}
-		if(data[setting]<=9){						// If the value is 1 digit we clean the 2nd one
-			lcd_gotoxy(1,1);						// By putting a space at 1,1 position
+		if(data[setting]<=9){					// If the value is 1 digit we clean the 2nd one
+			lcd_gotoxy(1,1);				// By putting a space at 1,1 position
 			lcd_puts(" ");
 		}
-		lcd_gotoxy(0,0);							// We got to the 0,0 position
+		lcd_gotoxy(0,0);					// We got to the 0,0 position
 		lcd_puts("Atmos Pressure:");				// We write the text
-		lcd_gotoxy(5,1);							// We write the unit in the 5,1 position
+		lcd_gotoxy(5,1);					// We write the unit in the 5,1 position
 		lcd_puts("KPa");
 		break;
 		
-		case 4:										// Humidity
-		data[4] = HumidGetValue(data, FULL);		// Update the humidity calling to the function
-		lcd_gotoxy(0,0);							// We go to 0,0
-		lcd_puts("Humid:");							// We write Humidity in that position
-		if(data[setting]>=100){						// If the value is higher than 100% is going to RAIN
-			lcd_gotoxy(0,1);						// We write a message in the 0,1 position
+		case 4:							// Humidity
+		data[4] = HumidGetValue(data, FULL);			// Update the humidity calling to the function
+		lcd_gotoxy(0,0);					// We go to 0,0
+		lcd_puts("Humid:");					// We write Humidity in that position
+		if(data[setting]>=100){					// If the value is higher than 100% is going to RAIN
+			lcd_gotoxy(0,1);				// We write a message in the 0,1 position
 			lcd_puts("RAIN");
-			lcd_gotoxy(5,1);						// We clean the unit
+			lcd_gotoxy(5,1);				// We clean the unit
 			lcd_puts(" ");
-		}else{										// If not we write that value
+		}else{							// If not we write that value
 		itoa(data[setting],lcd_string,10);			// We convert the value into a string
-		lcd_gotoxy(3,1);							// As the value is always gong to be less than 3 digits we clean that digit
-		lcd_puts(" ");								// We use a space
+		lcd_gotoxy(3,1);					// As the value is always gong to be less than 3 digits we clean that digit
+		lcd_puts(" ");						// We use a space
 		lcd_gotoxy(0,1);
-		lcd_puts(lcd_string);						// We write the string in the 0,1 position
-		if(data[setting]<=99){						// If the humidity is less than 99%
-		lcd_gotoxy(2,1);							// We clean the 2,1 position with a space
+		lcd_puts(lcd_string);					// We write the string in the 0,1 position
+		if(data[setting]<=99){					// If the humidity is less than 99%
+		lcd_gotoxy(2,1);					// We clean the 2,1 position with a space
 		lcd_puts(" ");
 		}
-		if(data[setting]<=9){						// If the humidity is less than 9%
-		lcd_gotoxy(1,1);							// We clean the 1,1 position with a space
+		if(data[setting]<=9){					// If the humidity is less than 9%
+		lcd_gotoxy(1,1);					// We clean the 1,1 position with a space
 		lcd_puts(" ");
 		}
-		lcd_gotoxy(5,1);							// We got to the position 5,1
-		lcd_puts("%");								// We write the unit
+		lcd_gotoxy(5,1);					// We got to the position 5,1
+		lcd_puts("%");						// We write the unit
 		}
 		break;
 		
-		case 5:												// Pressure tank
-		data[3] = PressureGetValue(data, FULL);				// Update the pressure of the atmosphere
-		data[0] = DistanceSensorValue(FULL);				// Update the distance
+		case 5:						// Pressure tank
+		data[3] = PressureGetValue(data, FULL);			// Update the pressure of the atmosphere
+		data[0] = DistanceSensorValue(FULL);			// Update the distance
 		uint16_t relative = data[3]+(data[0]*9.8/100000);	// By using Bernoulli we calculate the pressure of the bottom of the tank
-		itoa(relative,lcd_string,10);						// We convert it into a string
-		lcd_puts(lcd_string);								// We write it in the 0,1 position
-		if(data[setting]<=999){								// Like previously done we clean the digits depending on the values
+		itoa(relative,lcd_string,10);				// We convert it into a string
+		lcd_puts(lcd_string);					// We write it in the 0,1 position
+		if(data[setting]<=999){					// Like previously done we clean the digits depending on the values
 			lcd_gotoxy(3,1);
 			lcd_puts(" ");
 		}
